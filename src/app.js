836e4eaceeb0c0b8e6e5c549a2e2a30063bd0801
Francisco <francisco.cavasan12@gmail.com>
2023-09-24 17:19:05 -0300

    feat: add endpoint to populate remaining tables

--- src/app.js
+++ src/app.js
@@ -1,202 +1,397 @@
 // Import required modules
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const db = require("./dbConnection");
-const Faker = require('@faker-js/faker');
+const Faker = require("@faker-js/faker");
 
 // Create an Express application
 const app = express();
 
-
 function formatDateToYYYYMMDD(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
 
-    return `${year}-${month}-${day}`;
+  return `${year}-${month}-${day}`;
 }
 
 function getRandomDate(startYear, endYear) {
-    const startTimestamp = new Date(startYear, 0, 1).getTime();
-    const endTimestamp = new Date(endYear, 11, 31).getTime();
+  const startTimestamp = new Date(startYear, 0, 1).getTime();
+  const endTimestamp = new Date(endYear, 11, 31).getTime();
 
-    const randomTimestamp = startTimestamp + Math.random() * (endTimestamp - startTimestamp);
-    const randomDate = new Date(randomTimestamp);
+  const randomTimestamp =
+    startTimestamp + Math.random() * (endTimestamp - startTimestamp);
+  const randomDate = new Date(randomTimestamp);
 
-    return formatDateToYYYYMMDD(randomDate);
+  return formatDateToYYYYMMDD(randomDate);
 }
 
-async function getPKRange(pk, tabela){
-    const minCodResultQuery = `SELECT MIN(${pk}) AS min FROM ${tabela}`;
-    const maxCodResultQuery = `SELECT MAX(${pk}) AS max FROM ${tabela}`;
+async function getPKRange(pk, tabela) {
+  const minCodResultQuery = `SELECT MIN(${pk}) AS min FROM ${tabela}`;
+  const maxCodResultQuery = `SELECT MAX(${pk}) AS max FROM ${tabela}`;
 
-    const [minResult, maxResult] = await Promise.all([
+  const [minResult, maxResult] = await Promise.all([
     db.query(minCodResultQuery),
     db.query(maxCodResultQuery),
-    ]);
+  ]);
 
-    return { min: minResult.rows[0].min, max: maxResult.rows[0].max }
-} 
+  return { min: minResult.rows[0].min, max: maxResult.rows[0].max };
+}
 
 // Middleware to parse JSON requests
 app.use(bodyParser.text());
 
 // POST route
-app.post('/api/submit', async (req, res)  => {
-  
-    const data = await req.body;
-    console.log(data)
-    const result = await db.query(req.body)
+app.post("/api/submit", async (req, res) => {
+  const data = await req.body;
+  console.log(data);
+  const result = await db.query(req.body);
 
-    res.status(200).json({ result });
+  res.status(200).json({ result });
 });
 
-app.post('/populate/professor', async (req, res) =>  {
+app.post("/populate/professor", async (req, res) => {
+  let sql = "insert into professor values ";
+  const max = 10000;
 
-    let sql = 'insert into professor values '
-    const max = 10000
+  const { rows } = await db.query(
+    "SELECT cod_prof FROM professor ORDER BY cod_prof DESC LIMIT 1"
+  );
+  const lastId = rows.length > 0 ? rows[0].cod_prof + 1 : 1;
 
-    const { rows } = await db.query('SELECT cod_prof FROM professor ORDER BY cod_prof DESC LIMIT 1');
-    const lastId = rows.length > 0 ? rows[0].cod_prof + 1 : 1;
+  for (let i = lastId; i <= lastId + max; i++) {
+    const fullName = Faker.faker.person.fullName().replace(/'/g, "");
+    const streetAddress = Faker.faker.location
+      .streetAddress({ useFullAddress: true })
+      .replace(/'/g, "");
+    const phone = Faker.faker.helpers.replaceSymbolWithNumber("+55 9####-####");
 
+    sql += `(${i}, '${fullName}', '${streetAddress}', '${phone}')`;
+    if (i < lastId + max) {
+      sql += ", ";
+    }
+  }
 
-    for( let i = lastId; i <= (lastId + max); i++){
+  console.log(sql);
 
-        const fullName = Faker.faker.person.fullName().replace(/'/g, '');
-        const streetAddress = Faker.faker.location.streetAddress({ useFullAddress: true }).replace(/'/g, '');
-        const phone =  Faker.faker.helpers.replaceSymbolWithNumber('+55 9####-####');
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
 
-        sql += `(${i}, '${fullName}', '${streetAddress}', '${phone}')`
-            if (i < (lastId + max)) {
-                sql += ', ';
-            }
-    } 
-    
-    console.log(sql)
+app.post("/populate/aluno", async (req, res) => {
+  let sql = "insert into aluno values ";
+  const max = 500;
 
-    await db.query(sql);
-    res.status(200).send('Dados populados com sucesso.');
+  const { rows } = await db.query(
+    "SELECT cod_alu FROM aluno ORDER BY cod_alu DESC LIMIT 1"
+  );
+  const lastId = rows.length > 0 ? rows[0].cod_alu + 1 : 1;
 
-})
+  const pkRange = await getPKRange("cod_resultado", "resultado");
 
-app.post('/populate/aluno', async (req, res) =>  {
+  for (let i = lastId; i <= lastId + max; i++) {
+    const fullName = Faker.faker.person.fullName().replace(/'/g, "");
+    const streetAddress = Faker.faker.location
+      .streetAddress({ useFullAddress: true })
+      .replace(/'/g, "");
+    const dataNasc = getRandomDate(2005, 2010);
+    const cpf = Faker.faker.helpers.replaceSymbolWithNumber("###.###.###-##");
+    const rg = Faker.faker.helpers.replaceSymbolWithNumber("#######");
+    const cod_resultado = Faker.faker.helpers.rangeToNumber(pkRange);
 
-    let sql = 'insert into aluno values '
-    const max = 5
+    sql += `(${i}, '${fullName}', '${streetAddress}', '${dataNasc}', '${cpf}', '${rg}', '${cod_resultado}')`;
+    if (i < lastId + max) {
+      sql += ", ";
+    }
+  }
 
-    const { rows } = await db.query('SELECT cod_alu FROM aluno ORDER BY cod_alu DESC LIMIT 1');
-    const lastId = rows.length > 0 ? rows[0].cod_alu + 1 : 1;
+  console.log(sql);
 
-    const pkRange = await getPKRange('cod_resultado', 'resultado');
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
 
-    for( let i = lastId; i <= (lastId + max); i++){
+app.post("/populate/resultado", async (req, res) => {
+  let sql = "insert into resultado values ";
+  const max = 100000;
 
-        const fullName = Faker.faker.person.fullName().replace(/'/g, '');
-        const streetAddress = Faker.faker.location.streetAddress({ useFullAddress: true }).replace(/'/g, '');
-        const dataNasc = getRandomDate(2005, 2010);
-        const cpf =  Faker.faker.helpers.replaceSymbolWithNumber('###.###.###-##');
-        const rg =  Faker.faker.helpers.replaceSymbolWithNumber('#######');
-        const cod_resultado = Faker.faker.helpers.rangeToNumber(pkRange)
+  const { rows } = await db.query(
+    "SELECT cod_resultado FROM resultado ORDER BY cod_resultado DESC LIMIT 1"
+  );
+  const lastId = rows.length > 0 ? rows[0].cod_resultado + 1 : 1;
 
-        sql += `(${i}, '${fullName}', '${streetAddress}', '${dataNasc}', '${cpf}', '${rg}', '${cod_resultado}')`
-            if (i < (lastId + max)) {
-                sql += ', ';
-            }
-    } 
-    
-    console.log(sql)
+  const materiaPKRange = await getPKRange("cod_materia", "materia");
+  const semestrePKRange = await getPKRange("cod_semestre", "semestre");
 
-    await db.query(sql);
-    res.status(200).send('Dados populados com sucesso.');
-})
+  for (let i = lastId; i <= lastId + max; i++) {
+    const resutado = Faker.faker.helpers.rangeToNumber({ min: 2, max: 10 });
+    const cod_materia = Faker.faker.helpers.rangeToNumber(materiaPKRange);
+    const cod_semestre = Faker.faker.helpers.rangeToNumber(semestrePKRange);
 
-app.post('/populate/resultado', async (req, res) =>  {
+    sql += `(${i}, '${resutado}', '${cod_materia}', '${cod_semestre}')`;
 
-    let sql = 'insert into resultado values '
-    const max = 100000
+    if (i < lastId + max) {
+      sql += ", ";
+    }
+  }
 
-    const { rows } = await db.query('SELECT cod_resultado FROM resultado ORDER BY cod_resultado DESC LIMIT 1');
-    const lastId = rows.length > 0 ? rows[0].cod_resultado + 1 : 1;
+  console.log(sql);
 
-    const materiaPKRange = await getPKRange('cod_materia', 'materia');
-    const semestrePKRange = await getPKRange('cod_semestre', 'semestre');
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
 
-    for( let i = lastId; i <= (lastId + max); i++){
+app.post("/populate/materia", async (req, res) => {
+  let sql = "insert into materia values ";
+  const max = 100;
 
+  const { rows } = await db.query(
+    "SELECT cod_materia FROM resultado ORDER BY cod_materia DESC LIMIT 1"
+  );
+  const lastId = rows.length > 0 ? rows[0].cod_materia + 1 : 1;
 
-        const resutado = Faker.faker.helpers.rangeToNumber({min: 2, max: 10})
-        const cod_materia = Faker.faker.helpers.rangeToNumber(materiaPKRange)
-        const cod_semestre = Faker.faker.helpers.rangeToNumber(semestrePKRange)
-        
-        sql += `(${i}, '${resutado}', '${cod_materia}', '${cod_semestre}')`
-      
-        if (i < (lastId + max)) {
-            sql += ', ';
-        }
-    } 
-    
-    // console.log(sql)
+  for (let i = lastId; i <= lastId + max; i++) {
+    const materia = Faker.faker.lorem.words({ min: 1, max: 2 });
 
-    await db.query(sql);
-    res.status(200).send('Dados populados com sucesso.');
-})
+    sql += `(${i}, '${materia}')`;
 
-app.post('/populate/materia', async (req, res) =>  {
+    if (i < lastId + max) {
+      sql += ", ";
+    }
+  }
 
-    let sql = 'insert into materia values '
-    const max = 100
+  console.log(sql);
 
-    const { rows } = await db.query('SELECT cod_materia FROM resultado ORDER BY cod_materia DESC LIMIT 1');
-    const lastId = rows.length > 0 ? rows[0].cod_materia + 1 : 1;
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
 
-    for( let i = lastId; i <= (lastId + max); i++){
+app.post("/populate/semestre", async (req, res) => {
+  let sql = "insert into semestre values ";
+  const max = 20;
+  let ano = 2013;
+  let letra = "A";
 
-        const materia = Faker.faker.lorem.words({min: 1, max: 2});
-        
-        sql += `(${i}, '${materia}')`
-      
-        if (i < (lastId + max)) {
-            sql += ', ';
-        }
-    } 
-    
-    console.log(sql)
+  for (let i = 1; i <= max; i++) {
+    if (i % 2 != 0) {
+      ano++;
+      letra = "A";
+    } else {
+      letra = "B";
+    }
 
-    await db.query(sql);
-    res.status(200).send('Dados populados com sucesso.');
-})
+    sql += `(${i}, ${ano}, '${letra}')`;
 
-app.post('/populate/semestre', async (req, res) =>  {
+    if (i < max) {
+      sql += ", ";
+    }
+  }
 
-    let sql = 'insert into semestre values '
-    const max = 20
-    let ano = 2013;
-    let letra = 'A';
+  console.log(sql);
 
-    for( let i = 1; i <= max; i++){
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
 
-        if(i % 2 != 0){
-            ano++;
-            letra = 'A';
-        } else {
-            letra = 'B';
-        }
+app.post("/populate/turma", async (req, res) => {
+  let sql = "insert into turma values ";
+  const max = 40;
+  const professorPkRange = await getPKRange("cod_prof", "professor");
 
-        sql += `(${i}, ${ano}, '${letra}')`
-      
-        if (i < max) {
-            sql += ', ';
-        }
-    } 
-    
-    console.log(sql)
+  for (let i = 1; i <= max; i++) {
+    const description = Faker.faker.lorem.words({ min: 3, max: 5 });
+    const course = Faker.faker.lorem.words({ min: 1, max: 2 });
+    const initialDate = getRandomDate(2013, 2020);
+    const cod_professor = Faker.faker.helpers.rangeToNumber(professorPkRange);
 
-    await db.query(sql);
-    res.status(200).send('Dados populados com sucesso.');
-})
+    sql += `(${i}, '${description}', '${course}', '${initialDate}', ${cod_professor})`;
+
+    if (i < max) {
+      sql += ", ";
+    }
+  }
+
+  console.log(sql);
+
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
+
+app.post("/populate/prova", async (req, res) => {
+  let sql = "insert into prova values ";
+  const max = 200;
+  const materiaPkRange = await getPKRange("cod_materia", "materia");
+  const turmaPkRange = await getPKRange("cod_turma", "turma");
+
+  for (let i = 1; i <= max; i++) {
+    const date = getRandomDate(2013, 2020);
+    const subject = Faker.faker.lorem.words({ min: 1, max: 2 });
+    const weight = Faker.faker.helpers.rangeToNumber({ min: 1, max: 10 });
+    const cod_materia = Faker.faker.helpers.rangeToNumber(materiaPkRange);
+    const cod_turma = Faker.faker.helpers.rangeToNumber(turmaPkRange);
+
+    sql += `(${i}, '${date}', '${cod_turma}', '${subject}', '${weight}', ${cod_materia})`;
+
+    if (i < max) {
+      sql += ", ";
+    }
+  }
+
+  console.log(sql);
+
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
+
+app.post("/populate/contrato", async (req, res) => {
+  let sql = "insert into contrato values ";
+  const semestres = await db.query("SELECT cod_semestre FROM semestre");
+  const alunos = await db.query("SELECT cod_aluno FROM aluno");
+  let usedTurmas = [-1];
+  let count = 1;
+
+  for (let semestre of semestres.rows) {
+    const turmas = await db.query(
+      "SELECT cod_turma FROM turma WHERE cod_turma NOT IN (" +
+        usedTurmas.join(", ") +
+        ") ORDER BY RANDOM() LIMIT 5"
+    );
+
+    turmas.rows.forEach((turma) => {
+      alunos.rows.forEach((aluno) => {
+        const value = Faker.faker.finance.amount({
+          min: 100,
+          max: 3000,
+          precision: 2,
+        });
+
+        sql += `(${count}, ${aluno.cod_aluno}, ${turma.cod_turma}, ${value}, ${semestre.cod_semestre}), `;
+
+        count++;
+      });
+
+      usedTurmas.push(turma.cod_turma);
+    });
+  }
+
+  sql = sql.slice(0, -2);
+
+  console.log(sql);
+
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
+
+app.post("/populate/nota", async (req, res) => {
+  let sql = "insert into nota values ";
+  const exams = await db.query("SELECT * FROM prova");
+
+  for (let exam of exams.rows) {
+    const students = await db.query(
+      "SELECT cod_aluno FROM contrato WHERE cod_turma = " + exam.cod_turma
+    );
+
+    students.rows.forEach((student) => {
+      const grade = Faker.faker.helpers.rangeToNumber({ min: 0, max: 10 });
+
+      sql += `(${exam.cod_prova}, ${student.cod_aluno}, ${grade}), `;
+    });
+  }
+
+  sql = sql.slice(0, -2);
+
+  console.log(sql);
+
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
+
+app.post("/populate/aula", async (req, res) => {
+  let sql = "insert into aula values ";
+  const turmas = await db.query("SELECT * FROM turma");
+  const materiaPkRange = await getPKRange("cod_materia", "materia");
+
+  for (let turma of turmas.rows) {
+    let usedDates = [];
+    for (let i = 0; i < 10; i++) {
+      let date = getRandomDate(2013, 2023);
+      while (usedDates.includes(date)) {
+        date = getRandomDate(2013, 2023);
+      }
+
+      const subject = Faker.faker.lorem.words({ min: 1, max: 2 });
+      const hours = Faker.faker.helpers.rangeToNumber({ min: 1, max: 4 });
+      const cod_materia = Faker.faker.helpers.rangeToNumber(materiaPkRange);
+
+      sql += `(${turma.cod_turma}, '${date}', ${turma.cod_prof}, '${subject}', ${hours}, ${cod_materia}), `;
+
+      usedDates.push(date);
+    }
+  }
+
+  sql = sql.slice(0, -2);
+
+  console.log(sql);
+
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
+
+app.post("/populate/presenca", async (req, res) => {
+  let sql = "insert into presenca values ";
+  const aulas = await db.query("SELECT * FROM aula");
+
+  for (let aula of aulas.rows) {
+    const alunos = await db.query(
+      "SELECT cod_aluno FROM contrato WHERE cod_turma = " + aula.cod_turma
+    );
+
+    alunos.rows.forEach((aluno) => {
+      sql += `(${aula.cod_turma}, '${formatDateToYYYYMMDD(aula.data_aula)}', ${
+        aluno.cod_aluno
+      }), `;
+    });
+  }
+
+  sql = sql.slice(0, -2);
+
+  console.log(sql);
+
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
+
+app.post("/populate/parcela", async (req, res) => {
+  let sql = "insert into parcela values ";
+  const contratos = await db.query("SELECT * FROM contrato");
+
+  for (let contrato of contratos.rows) {
+    const dueDate = getRandomDate(2013, 2023);
+    const value = Faker.faker.finance.amount({
+      min: 100,
+      max: 3000,
+      precision: 2,
+    });
+    const paymentDate = getRandomDate(2013, 2023);
+    const paidAmount = Faker.faker.finance.amount({
+      min: 100,
+      max: 3000,
+      precision: 2,
+    });
+
+    sql += `(${contrato.cod_contrato}, '${dueDate}', ${value}, '${paymentDate}', ${paidAmount}), `;
+  }
+
+  sql = sql.slice(0, -2);
+
+  console.log(sql);
+
+  await db.query(sql);
+  res.status(200).send("Dados populados com sucesso.");
+});
 
 // Start the Express server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
